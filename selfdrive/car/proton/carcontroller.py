0ddadb4a372fd7b2ca0a77d370ce2b49441d45fe
iXcess <brandonting@hotmail.com>
2024-09-11 17:50:08 +0800

    [Untested] Proton Support

---
+++ selfdrive/car/proton/carcontroller.py
@@ -0,0 +1,103 @@
+from cereal import car
+from opendbc.can.packer import CANPacker
+
+from openpilot.selfdrive.car.interfaces import CarControllerBase
+from openpilot.selfdrive.car.proton.protoncan import create_can_steer_command, create_hud, create_lead_detect, send_buttons, create_acc_cmd
+from openpilot.selfdrive.car.proton.values import CAR, DBC
+from openpilot.common.numpy_fast import clip, interp
+from openpilot.common.realtime import DT_CTRL
+from openpilot.common.params import Params
+
+
+def apply_proton_steer_torque_limits(apply_torque, apply_torque_last, driver_torque, LIMITS):
+
+  # limits due to driver torque
+  driver_max_torque = LIMITS.STEER_MAX + driver_torque * 30
+  driver_min_torque = -LIMITS.STEER_MAX + driver_torque * 30
+  max_steer_allowed = max(min(LIMITS.STEER_MAX, driver_max_torque), 0)
+  min_steer_allowed = min(max(-LIMITS.STEER_MAX, driver_min_torque), 0)
+  apply_torque = clip(apply_torque, min_steer_allowed, max_steer_allowed)
+
+  # slow rate if steer torque increases in magnitude
+  if apply_torque_last > 0:
+    apply_torque = clip(apply_torque, max(apply_torque_last - LIMITS.STEER_DELTA_DOWN, -LIMITS.STEER_DELTA_UP),
+                        apply_torque_last + LIMITS.STEER_DELTA_UP)
+  else:
+    apply_torque = clip(apply_torque, apply_torque_last - LIMITS.STEER_DELTA_UP,
+                        min(apply_torque_last + LIMITS.STEER_DELTA_DOWN, LIMITS.STEER_DELTA_UP))
+
+  return int(round(float(apply_torque)))
+
+class CarControllerParams():
+  def __init__(self, CP):
+
+    self.STEER_MAX = CP.lateralParams.torqueV[0]
+    # make sure Proton only has one max steer torque value
+    assert(len(CP.lateralParams.torqueV) == 1)
+
+    # for torque limit calculation
+    self.STEER_DELTA_UP = 20                      # torque increase per refresh, 0.8s to max
+    self.STEER_DELTA_DOWN = 30                    # torque decrease per refresh
+
+class CarController(CarControllerBase):
+  def __init__(self, dbc_name, CP, VM):
+    self.CP = CP
+    self.frame = 0
+    self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
+    self.params = CarControllerParams(self.CP)
+
+    self.last_steer = 0
+    self.steer_rate_limited = False
+    self.steering_direction = False
+    self.num_cruise_btn_sent = 0
+
+  def update(self, CC, CS, now_nanos):
+    can_sends = []
+
+    enabled = CC.latActive
+    latActive = enabled
+    actuators = CC.actuators
+    lead_visible = CC.hudControl.leadVisible
+    rlane_visible = CC.hudControl.rightLaneVisible
+    llane_visible = CC.hudControl.leftLaneVisible
+    ldw = CC.hudControl.leftLaneDepart or CC.hudControl.rightLaneDepart
+    
+    # TODO laneActive, used to check if ALC is off
+    lat_active = enabled and not CS.lkaDisabled
+
+    # TODO what is this for?
+    if self.frame <= 1000 and CS.out.cruiseState.available and self.num_cruise_btn_sent <= 5:
+      self.num_cruise_btn_sent += 1
+      can_sends.append(send_buttons(self.packer, self.frame % 16, True))
+
+    # steer
+    new_steer = int(round(actuators.steer * self.params.STEER_MAX))
+    # TODO use openpilot's ready function
+    apply_steer = apply_proton_steer_torque_limits(new_steer, self.last_steer, 0, self.params)
+
+    ts = self.frame * DT_CTRL
+
+    # CAN controlled lateral running at 50hz
+    if (self.frame % 2) == 0:
+      if CS.stock_ldp:
+        steer_dir = -1 if CS.steer_dir else 1
+        apply_steer = CS.stock_ldp_cmd * steer_dir * 0.0002
+        lat_active |= True
+
+      can_sends.append(create_can_steer_command(self.packer, apply_steer, lat_active, CS.hand_on_wheel_warning and CS.is_icc_on, (self.frame/2) % 16, CS.stock_lks_settings,  CS.stock_lks_settings2))
+
+      #can_sends.append(create_hud(self.packer, apply_steer, enabled, ldw, rlane_visible, llane_visible))
+      #can_sends.append(create_lead_detect(self.packer, lead_visible, enabled))
+      #can_sends.append(create_acc_cmd(self.packer, actuators.accel, fake_enable, (frame/2) % 16))
+
+    if CS.out.standstill and enabled and (self.frame % 50 == 0):
+      # Spam resume button to resume from standstill at max freq of 10 Hz.
+      if CS.acc_req:
+        can_sends.append(send_buttons(self.packer, self.frame % 16, False))
+
+    self.last_steer = apply_steer
+    new_actuators = actuators.copy()
+    new_actuators.steer = apply_steer / self.params.STEER_MAX
+
+    self.frame += 1
+    return new_actuators, can_sends
---
+++ selfdrive/car/proton/interface.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+from cereal import car
+from openpilot.selfdrive.car import STD_CARGO_KG, get_safety_config
+from openpilot.selfdrive.car.interfaces import CarInterfaceBase
+from openpilot.selfdrive.car.proton.values import CAR
+from openpilot.selfdrive.controls.lib.desire_helper import LANE_CHANGE_SPEED_MIN
+
+from common.params import Params
+
+EventName = car.CarEvent.EventName
+
+class CarInterface(CarInterfaceBase):
+
+  @staticmethod
+  def _get_params(ret, candidate, fingerprint, car_fw, experimental_long, docs):
+    ret.carName = "proton"
+
+    ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.proton)]
+    ret.safetyConfigs[0].safetyParam = 1   # TODO: add actual safetyParam
+
+    ret.steerControlType = car.CarParams.SteerControlType.torque
+    ret.steerLimitTimer = 0.1              # time before steerLimitAlert is issued
+    ret.steerActuatorDelay = 0.30          # Steering wheel actuator delay in seconds
+
+    ret.lateralTuning.init('pid')
+
+    ret.lateralTuning.pid.kpBP = [0., 25., 35., 40.]
+    ret.lateralTuning.pid.kpV = [0.05, 0.15, 0.15, 0.16]
+    ret.lateralTuning.pid.kiBP = [0., 20., 30.]
+    ret.lateralTuning.pid.kiV = [0.10, 0.20, 0.40]
+    ret.lateralTuning.pid.kf = 0.00007
+
+    ret.longitudinalTuning.kpBP = [0., 5., 20.]
+    ret.longitudinalTuning.kpV = [0, 0, 0]
+    ret.longitudinalActuatorDelayLowerBound = 0.42
+    ret.longitudinalActuatorDelayUpperBound = 0.60
+    ret.longitudinalTuning.deadzoneBP = [0., 8.05]
+    ret.longitudinalTuning.deadzoneV = [0, 0]
+    ret.longitudinalTuning.kiBP = [0., 5., 20.]
+    ret.longitudinalTuning.kiV = [0, 0, 0]
+
+    ret.centerToFront = ret.wheelbase * 0.44
+    ret.tireStiffnessFactor = 0.7933
+
+    ret.openpilotLongitudinalControl = True
+
+    if candidate == CAR.X50:
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [530]]
+    elif candidate == CAR.S70:
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [530]]
+    elif candidate == CAR.X90:
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0.], [530]]
+    else:
+      ret.dashcamOnly = True
+      ret.safetyModel = car.CarParams.SafetyModel.noOutput
+
+    ret.minEnableSpeed = -1
+    ret.enableBsm = True
+    ret.stoppingDecelRate = 0.001 # reach stopping target smoothly
+
+    return ret
+
+  # returns a car.CarState
+  def _update(self, c):
+    ret = self.CS.update(self.cp)
+
+    # events
+    events = self.create_common_events(ret)
+
+    # TODO: if self.CS.hand_on_wheel_warning and self.CS.is_icc_on:
+    #  events.add(EventName.protonHandOnWheelWarning)
+
+    ret.events = events.to_msg()
+    return ret
+
+  # pass in a car.CarControl to be called at 100hz
+  def apply(self, c, now_nanos):
+    return self.CC.update(c, self.CS, now_nanos)
