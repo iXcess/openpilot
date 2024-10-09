2ca5768714c8f8cb0c0fc70c27d4f1736c1fdcb6
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2024-10-09 16:23:13 +0800

    Seding settings data over TCP

--- selfdrive/streamdatad/streamdatad.py
+++ selfdrive/streamdatad/streamdatad.py
@@ -5,10 +5,14 @@ import os
 
 from openpilot.common.realtime import Ratekeeper
 import cereal.messaging as messaging
+from cereal import log
+from openpilot.system.version import get_version, get_commit, get_short_branch
+from openpilot.common.params import Params
 
 BUFFER_SIZE = 1024
 UDP_PORT = 5006
 TCP_PORT = 5007
+params = Params()
 
 def get_wlan_ip():
   for iface in os.listdir('/sys/class/net/'):
@@ -29,14 +33,29 @@ class Streamer:
     self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.tcp_conn = None
-    self.sm = sm if sm else messaging.SubMaster(['navInstruction', 'settings'])
+    self.sm = sm if sm \
+      else messaging.SubMaster(['navInstruction', 'settings', 'deviceState', 'peripheralState',\
+      'controlsState', 'uploaderState'])
     self.rk = Ratekeeper(10)  # Ratekeeper for 10 Hz loop
 
     self.setup_sockets()
 
+  def deviceStatus(self):
+    if self.sm['peripheralState'].pandaType == log.PandaState.PandaType.unknown:
+      return "error"
+    else: # TODO: Add initialising if alerts.hasSevere from k_alerts
+      return "ready"
+
+  def remainingDataUpload(self):
+    uploader_state = self.sm['uploaderState']
+    immediate_queue_size = uploader_state.immediateQueueSize
+    raw_queue_size = uploader_state.rawQueueSize
+    return f"{immediate_queue_size + raw_queue_size} MB"
+
   def setup_sockets(self):
     self.udp_sock.bind((self.local_ip, UDP_PORT))
     self.udp_sock.setblocking(False)
+    self.tcp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Enable reuse for TCP socket
     self.tcp_sock.bind((self.local_ip, TCP_PORT))
     self.tcp_sock.listen(1)
     self.tcp_sock.setblocking(False)
@@ -50,10 +69,48 @@ class Streamer:
   def send_tcp_message(self):
     if self.tcp_conn:
       try:
-        settings_builder = self.sm['settings'].as_builder()
-        settings_builder.key = "dummy_key"
-        settings_builder.value = "dummy_value"
-        self.tcp_conn.sendall(settings_builder.to_bytes())
+        sett = self.sm['settings'].as_builder()
+
+        sett.connectivityStatus = str(self.sm['deviceState'].networkType)
+        sett.deviceStatus = self.deviceStatus()
+        sett.alerts = (str(self.sm['controlsState'].alertText1) + "\n" + str(self.sm['controlsState'].alertText2))
+        sett.remainingDataUpload = self.remainingDataUpload()
+        # TODO send uploadStatus in selfdrive/loggerd/uploader.py
+        sett.gitCommit = get_commit()
+        # TODO include bukapilot params.cc
+        # sett.updateStatus = params.get("UpdateStatus")
+
+        settings_open = True
+        if settings_open:
+          sett.enableBukapilot = params.get_bool("OpenpilotEnabledToggle")
+          # sett.quietMode = params.get_bool("QuietMode")
+          # sett.enableAssistedLaneChange = params.get_bool("IsAlcEnabled")
+          sett.enableLaneDepartureWarning = params.get_bool("IsLdwEnabled")
+          # sett.uploadVideoWiFiOnly = params.get_bool("LogVideoWifiOnly")
+          sett.apn = params.get("GsmApn") or ''
+          sett.enableRoaming = params.get_bool("GsmRoaming")
+          sett.driverPersonality = params.get("LongitudinalPersonality")
+          sett.useMetricSystem = params.get_bool("IsMetric")
+          sett.enableSSH = params.get_bool("SshEnabled")
+          sett.experimentalModel = params.get_bool("ExperimentalMode")
+          # sett.stopDistanceOffset = params.get("StoppingDistanceOffset")
+          # sett.pathSkewOffset = params.get("DrivePathOffset")
+          # sett.devicePowerOffTime = params.get("PowerSaverEntryDuration")
+          # TODO: sett.changeBranchStatus =
+          # sett.featurePackage = params.get("FeaturesPackage")
+          # sett.fixFingerprint = params.get("FixFingerprint")
+
+        request_info = True
+        if request_info:
+          sett.dongleID = params.get("DongleId")
+          sett.serial = params.get("HardwareSerial") or ''
+          sett.ipAddress = get_wlan_ip()
+          sett.hostname = socket.gethostname()
+          sett.currentVersion = get_version()
+          sett.currentBranch = get_short_branch()
+          sett.currentChangelog = params.get("UpdaterCurrentReleaseNotes") or ''
+
+        self.tcp_conn.sendall(sett.to_bytes())
       except socket.error:
         self.tcp_conn = None  # Reset connection on error
 
