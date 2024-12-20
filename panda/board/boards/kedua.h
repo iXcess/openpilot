31148a064e7f90dccb2f00bd1bbb81b861115cd0
Penyelidik Kereta <penyelidik@kommu.ai>
2024-12-20 02:15:08 +0000

    [WIP] Soft reset needed after working

--- panda/board/boards/kedua.h
+++ panda/board/boards/kedua.h
@@ -8,13 +8,13 @@ void kedua_enable_can_transceiver(uint8_t transceiver, bool enabled) {
       set_gpio_output(GPIOG, 11, !enabled);
       break;
     case 2U:
-      set_gpio_output(GPIOB, 3, !enabled);
+      set_gpio_output(GPIOB, 11, !enabled);
       break;
     case 3U:
       set_gpio_output(GPIOD, 7, !enabled);
       break;
     case 4U:
-      set_gpio_output(GPIOB, 4, !enabled);
+      set_gpio_output(GPIOB, 10, !enabled);
       break;
     default:
       break;
@@ -101,45 +101,43 @@ bool kedua_check_ignition(void) {
 }
 
 uint32_t kedua_read_voltage_mV(void){
-  return adc_get_mV(2) * 11U; // TODO: is this correct?
+  return adc_get_mV(2) * 11U;
+}
+
+uint32_t kedua_read_current_mA(void){
+  return adc_get_mV(3) * 2U;
 }
 
 void kedua_init(void) {
   common_init_gpio();
 
-  //C10,C11 : OBD_SBU1_RELAY, OBD_SBU2_RELAY
-  set_gpio_output_type(GPIOC, 10, OUTPUT_TYPE_OPEN_DRAIN);
-  set_gpio_pullup(GPIOC, 10, PULL_NONE);
-  set_gpio_mode(GPIOC, 10, MODE_OUTPUT);
-  set_gpio_output(GPIOC, 10, 1);
+  //PA1,PC4 : OBD_SBU1_RELAY, OBD_SBU2_RELAY
+  set_gpio_output_type(GPIOC, 4, OUTPUT_TYPE_PUSH_PULL);
+  set_gpio_pullup(GPIOC, 4, PULL_NONE);
+  set_gpio_mode(GPIOC, 4, MODE_OUTPUT);
+  set_gpio_output(GPIOC, 4, 0);
 
-  set_gpio_output_type(GPIOC, 11, OUTPUT_TYPE_OPEN_DRAIN);
-  set_gpio_pullup(GPIOC, 11, PULL_NONE);
-  set_gpio_mode(GPIOC, 11, MODE_OUTPUT);
-  set_gpio_output(GPIOC, 11, 1);
+  set_gpio_output_type(GPIOA, 1, OUTPUT_TYPE_PUSH_PULL);
+  set_gpio_pullup(GPIOA, 1, PULL_NONE);
+  set_gpio_mode(GPIOA, 1, MODE_OUTPUT);
+  set_gpio_output(GPIOA, 1, 0);
 
-  // G11,B3,D7,B4: transceiver enable
+  // G11,B11,D7,B10: transceiver enable
   set_gpio_pullup(GPIOG, 11, PULL_NONE);
   set_gpio_mode(GPIOG, 11, MODE_OUTPUT);
 
-  set_gpio_pullup(GPIOB, 3, PULL_NONE);
-  set_gpio_mode(GPIOB, 3, MODE_OUTPUT);
+  set_gpio_pullup(GPIOB, 11, PULL_NONE);
+  set_gpio_mode(GPIOB, 11, MODE_OUTPUT);
 
   set_gpio_pullup(GPIOD, 7, PULL_NONE);
   set_gpio_mode(GPIOD, 7, MODE_OUTPUT);
 
-  set_gpio_pullup(GPIOB, 4, PULL_NONE);
-  set_gpio_mode(GPIOB, 4, MODE_OUTPUT);
+  set_gpio_pullup(GPIOB, 10, PULL_NONE);
+  set_gpio_mode(GPIOB, 10, MODE_OUTPUT);
 
   //B1: 5VOUT_S
   set_gpio_pullup(GPIOB, 1, PULL_NONE);
   set_gpio_mode(GPIOB, 1, MODE_ANALOG);
-
-  // B14: usb load switch, enabled by pull resistor on board, obsolete for kedua
-  set_gpio_output_type(GPIOB, 14, OUTPUT_TYPE_OPEN_DRAIN);
-  set_gpio_pullup(GPIOB, 14, PULL_UP);
-  set_gpio_mode(GPIOB, 14, MODE_OUTPUT);
-  set_gpio_output(GPIOB, 14, 1);
 
   // Initialize harness
   harness_init();
@@ -171,27 +169,27 @@ void kedua_init(void) {
 
 const harness_configuration kedua_harness_config = {
   .has_harness = true,
-  .GPIO_SBU1 = GPIOC,
-  .GPIO_SBU2 = GPIOA,
-  .GPIO_relay_SBU1 = GPIOC,
+  .GPIO_SBU1 = GPIOA,
+  .GPIO_SBU2 = GPIOC,
+  .GPIO_relay_SBU1 = GPIOA,
   .GPIO_relay_SBU2 = GPIOC,
-  .pin_SBU1 = 4,
-  .pin_SBU2 = 1,
-  .pin_relay_SBU1 = 10,
-  .pin_relay_SBU2 = 11,
-  .adc_channel_SBU1 = 4, //ADC12_INP4
-  .adc_channel_SBU2 = 17 //ADC1_INP17
+  .pin_SBU1 = 1,
+  .pin_SBU2 = 4,
+  .pin_relay_SBU1 = 1,
+  .pin_relay_SBU2 = 4,
+  .adc_channel_SBU1 = 17, //ADC12_INP4
+  .adc_channel_SBU2 = 4 //ADC1_INP17
 };
 
 const board board_kedua = {
   .set_bootkick = unused_set_bootkick, //TODO
-  .harness_config = &kedua_harness_config, 
+  .harness_config = &kedua_harness_config,
   .has_obd = true,
   .has_spi = true,
   .has_canfd = true,
   .has_rtc_battery = false, //TODO
   .fan_max_rpm = 0U,
-  .avdd_mV = 3300U, //TODO
+  .avdd_mV = 3300U,
   .fan_stall_recovery = false,
   .fan_enable_cooldown_time = 0U,
   .init = kedua_init,
@@ -201,8 +199,8 @@ const board board_kedua = {
   .set_led = kedua_set_led,
   .set_can_mode = kedua_set_can_mode,
   .check_ignition = kedua_check_ignition,
-  .read_voltage_mV = kedua_read_voltage_mV, //TODO
-  .read_current_mA = unused_read_current, //TODO
+  .read_voltage_mV = kedua_read_voltage_mV,
+  .read_current_mA = kedua_read_current_mA, //TODO
   .set_fan_enabled = unused_set_fan_enabled,
   .set_ir_power = kedua_set_ir_power,
   .set_siren = unused_set_siren,
