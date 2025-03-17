074a7b6d7595f11aaf48d63289667a9b88f6c65f
Penyelidik Kereta <penyelidik@kommu.ai>
2025-03-17 09:04:38 +0000

    Add more byd, dnga and proton stuff

--- panda/board/safety/safety_dnga.h
+++ panda/board/safety/safety_dnga.h
@@ -1,36 +1,43 @@
-const CanMsg DNGA_TX_MSGS[] = {{0x250, 0, 8}, {0x250, 0, 6}, {0x251, 0, 5},  // dnga
-                               {0x350, 0, 8}, {0x350, 0, 6}, {0x351, 0, 5},  // knee
-                               {0x1, 0, 8}}; // CAN flasher
+const CanMsg DNGA_TX_MSGS[] = {{0x250, 0, 8}};
 
 RxCheck dnga_rx_checks[] = {
-  {.msg = {{0x201, 0, 8, .check_checksum = false, .max_counter = 0U, .frequency = 100U}, { 0 }, { 0 }}},
+  //{.msg = {{0x35F, 0, 8, .frequency = 20U}, { 0 }, { 0 }}},
 };
 
 static void dnga_rx_hook(const CANPacket_t *to_push) {
   // dnga is never at standstill
   vehicle_moving = true;
-
-  if (GET_ADDR(to_push) == 0x201U) {
-    controls_allowed = true;
-  }
+  controls_allowed = true;
+  UNUSED(to_push);
 }
 
 static bool dnga_tx_hook(const CANPacket_t *to_send) {
   bool tx = true;
   int addr = GET_ADDR(to_send);
   int len = GET_LEN(to_send);
-
-  if (!controls_allowed && (addr != 0x1)) {
-    tx = false;
-  }
-
-  // Allow going into CAN flashing mode for base & knee even if controls are not allowed
-  bool flash_msg = ((addr == 0x250) || (addr == 0x350)) && (len == 8);
-  if (!controls_allowed && (GET_BYTES(to_send, 0, 4) == 0xdeadfaceU) && (GET_BYTES(to_send, 4, 4) == 0x0ab00b1eU) && flash_msg) {
-    tx = true;
-  }
+  UNUSED(addr);
+  UNUSED(len);
 
   return tx;
+}
+
+static int dnga_fwd_hook(int bus_num, int addr) {
+  int bus_fwd = -1;
+
+  if (bus_num == 0) {
+    bus_fwd = 2;
+  }
+
+  if (bus_num == 2) {
+    bool is_lkas_msg = ((addr == 464) || (addr == 628));
+    bool is_acc_msg = ((addr == 625) || (addr == 627));
+    bool block_msg = is_lkas_msg || is_acc_msg;
+    if (!block_msg) {
+      bus_fwd = 0;
+    }
+  }
+
+  return bus_fwd;
 }
 
 static safety_config dnga_init(uint16_t param) {
@@ -38,9 +45,10 @@ static safety_config dnga_init(uint16_t param) {
   return BUILD_SAFETY_CFG(dnga_rx_checks, DNGA_TX_MSGS);
 }
 
+
 const safety_hooks dnga_hooks = {
   .init = dnga_init,
   .rx = dnga_rx_hook,
   .tx = dnga_tx_hook,
-  .fwd = default_fwd_hook,
+  .fwd = dnga_fwd_hook,
 };
--- panda/board/safety/safety_proton.h
+++ panda/board/safety/safety_proton.h
@@ -1,36 +1,43 @@
-const CanMsg PROTON_TX_MSGS[] = {{0x250, 0, 8}, {0x250, 0, 6}, {0x251, 0, 5},  // proton
-                               {0x350, 0, 8}, {0x350, 0, 6}, {0x351, 0, 5},  // knee
-                               {0x1, 0, 8}}; // CAN flasher
+const CanMsg PROTON_TX_MSGS[] = {{0x250, 0, 8}};
 
 RxCheck proton_rx_checks[] = {
-  {.msg = {{0x201, 0, 8, .check_checksum = false, .max_counter = 0U, .frequency = 100U}, { 0 }, { 0 }}},
+  //{.msg = {{0x35F, 0, 8, .frequency = 20U}, { 0 }, { 0 }}},
 };
 
 static void proton_rx_hook(const CANPacket_t *to_push) {
   // proton is never at standstill
   vehicle_moving = true;
-
-  if (GET_ADDR(to_push) == 0x201U) {
-    controls_allowed = true;
-  }
+  controls_allowed = true;
+  UNUSED(to_push);
 }
 
 static bool proton_tx_hook(const CANPacket_t *to_send) {
   bool tx = true;
   int addr = GET_ADDR(to_send);
   int len = GET_LEN(to_send);
-
-  if (!controls_allowed && (addr != 0x1)) {
-    tx = false;
-  }
-
-  // Allow going into CAN flashing mode for base & knee even if controls are not allowed
-  bool flash_msg = ((addr == 0x250) || (addr == 0x350)) && (len == 8);
-  if (!controls_allowed && (GET_BYTES(to_send, 0, 4) == 0xdeadfaceU) && (GET_BYTES(to_send, 4, 4) == 0x0ab00b1eU) && flash_msg) {
-    tx = true;
-  }
+  UNUSED(addr);
+  UNUSED(len);
 
   return tx;
+}
+
+static int proton_fwd_hook(int bus_num, int addr) {
+  int bus_fwd = -1;
+
+  if (bus_num == 0) {
+    bus_fwd = 2;
+  }
+
+  if (bus_num == 2) {
+    bool is_lkas_msg = ((addr == 432));// || (addr == 790));
+    bool is_acc_msg = (addr == 417);
+    bool block_msg = is_lkas_msg || is_acc_msg;
+    if (!block_msg) {
+      bus_fwd = 0;
+    }
+  }
+
+  return bus_fwd;
 }
 
 static safety_config proton_init(uint16_t param) {
@@ -38,9 +45,10 @@ static safety_config proton_init(uint16_t param) {
   return BUILD_SAFETY_CFG(proton_rx_checks, PROTON_TX_MSGS);
 }
 
+
 const safety_hooks proton_hooks = {
   .init = proton_init,
   .rx = proton_rx_hook,
   .tx = proton_tx_hook,
-  .fwd = default_fwd_hook,
+  .fwd = proton_fwd_hook,
 };
