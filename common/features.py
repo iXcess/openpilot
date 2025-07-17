c7f0386f50ad5e75d58249a62f13bcb4e9738004
Chong Bin Wee <34395942+cbwee@users.noreply.github.com>
2025-07-17 17:57:15 +0800

    streamdatad and features handle missing params

--- common/features.py
+++ common/features.py
@@ -30,7 +30,7 @@ def _process_feature_string(feature_string_input: str) -> str:
 
 def _get_features_param() -> str:
   """Safely retrieves the feature parameter as a string."""
-  return _get_params().get("FeaturesPackage").decode() or ""
+  return (_get_params().get("FeaturesPackage") or b"").decode()
 
 def _put_features_param(value: str) -> None:
   """Puts the feature parameter value."""
